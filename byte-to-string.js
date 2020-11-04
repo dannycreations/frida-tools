afa40f4710119c4ae435db29a83a0021ff4ab164
Danny <usernamekepo@gmail.com>
2020-11-04 14:06:04 +0700

    Update byte-to-string.js

--- byte-to-string.js
+++ byte-to-string.js
@@ -1,7 +1,7 @@
 function byteToString(byte) {
-  const result = ''
-  const buffer = Java.array('byte', byte)
-  for (let i = 0; i < buffer.length; ++i){
+  var result = ''
+  var buffer = Java.array('byte', byte)
+  for (var i = 0; i < buffer.length; ++i) {
     result += (String.fromCharCode(buffer[i] & 0xff))
   }
   return result
